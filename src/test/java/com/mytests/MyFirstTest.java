2a2ea878593b4c063012ec93c87faabe905ea22e
Dima Parkhomenko <dimin13111986@gmail.com>
2017-07-29 10:59:49 +0300

    Changed pass to chromedriver

--- src/test/java/com/mytests/MyFirstTest.java
+++ src/test/java/com/mytests/MyFirstTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
@@ -19,7 +18,6 @@ public class MyFirstTest {
 
     @Before
     public void start() {
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 10);
 
@@ -27,11 +25,10 @@ public class MyFirstTest {
 
     @Test
     public void myFirstTest() {
-
-        driver.get("https://www.google.com");
-        driver.findElement(By.name("q")).sendKeys("webdriver");
-       /* driver.findElement(By.name("btnG")).click();
-        wait.until(titleIs("webdriver - Поиск в Google"));*/
+        driver.get("https://www.google.com.ua");
+        driver.findElement(By.name("q")).sendKeys("webdriver torso");
+        driver.findElement(By.cssSelector(".sbqs_c")).click();
+        wait.until(titleIs("webdriver torso - Поиск в Google"));
     }
 
     @After
